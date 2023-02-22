cceae03e13e46daece378eff22b913db3ca7b9d9
richardmundyiii <mundy.richard@icloud.com>
2023-02-21 16:43:57 -0800

    fix data special chars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import puppeteer from "puppeteer";
 
+// const mongoose = require("./model/standings");
+
 const url = "https://tv.dartconnect.com/league/SaCzDA/6210/standings";
 
 const main = async () => {
@@ -10,17 +12,19 @@ const main = async () => {
   let content = await page.evaluate(() => {
     let magicArr = [];
     let divs = [...document.querySelectorAll("tr")];
-    let formatted = divs.map((div) => div.innerText.split("."));
-    let bLeauge = formatted.splice(2, 8);
-    let aLeague = formatted.splice(4, 8);
+    let formatted = divs.map((div) => div.innerText.replace(/\t/g, " "));
+    // let bLeauge = formatted.splice(2, 8);
+    let aLeague = formatted.splice(2, 8);
 
-    aLeague[0][1].forEach((el) => {
-      magicArr.push(el);
-    });
-    return magicArr;
+    // aLeague.forEach((el) => {
+    //   magicArr.push(el);
+    // });
+    // return { bLeauge, aLeague };
+    return aLeague[0];
   });
   console.log(content);
   await browser.close();
 };
 
+// mongoose.save(content);
 main();
