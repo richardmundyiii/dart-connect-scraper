cea171de8526ccc555b96bb069e297bd37bafa07
richardmundyiii <mundy.richard@icloud.com>
2023-02-23 22:17:39 -0800

    README update

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 # Dart Connect Web Scrapper
 
-##### A web-based app to scrape league standings from Dart Connect's public league standings page. In turn, the scrapper will populate your own website's standing page while also saving the data to a MongoDB collection.
+##### A web-based app to scrape league standings from Dart Connect's public league standings page.
 
 </div>
+
+## :camera_flash: Screenshots
+
+|            Description             | Screenshot                                    |
+| :--------------------------------: | --------------------------------------------- |
+| <h3 align="center">Wireframes</h3> | <img src="images/dcScreen.png" width="700" /> |
+
+</div>
+
+<div>
--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-import puppeteer from "puppeteer";
-
-// const mongoose = require("./model/standings");
+const puppeteer = require("puppeteer");
+const mongoose = require("mongoose");
+const StandingA = require("../models/StandingA");
+require("dotenv").config();
+require("../config/database");
 
 const url = "https://tv.dartconnect.com/league/SaCzDA/6210/standings";
 
 const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
   await page.goto(url);
 
-  let content = await page.evaluate(() => {
-    let magicArr = [];
-    let divs = [...document.querySelectorAll("tr")];
-    let formatted = divs.map((div) => div.innerText.replace(/\t/g, " "));
-    // let bLeauge = formatted.splice(2, 8);
-    let aLeague = formatted;
-
-    // aLeague.forEach((el) => {
-    //   el.split(" ");
-    //   magicArr.push(el);
-    // });
-    return aLeague[2].split(" ");
-    // return aLeague[0][1];
+  const content = await page.evaluate(async () => {
+    const rows = [...document.querySelectorAll("tr")];
+    const formatted = rows.map((row, index) => {
+      const cells = [...row.querySelectorAll("td")];
+      const formattedCells = cells.map((cell) => {
+        return cell.innerText;
+      });
+      return formattedCells;
+    });
+    const aLeague = formatted.splice(12, 14);
+    return aLeague;
   });
-  console.log(content);
+
+  //   await Standing.deleteMany({});
+
+  await StandingA.insertMany(
+    content.map((row) => ({
+      place: Number(row[0]),
+      points: Number(row[1]),
+      matchesPlayed: Number(row[2]),
+      matchesWon: Number(row[3]),
+      teamName: row[4],
+      legWonPct: row[5],
+      zeroOneAvg: row[6],
+      cricketAvg: row[7],
+      league: "A",
+    }))
+  );
+
   await browser.close();
 };
 
-// mongoose.save(content);
 main();
